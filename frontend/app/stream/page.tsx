20bbdc95bc6b5bd25a971909bfd40f565367ba7c
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2024-06-15 16:24:13 +0530

    Frame extraction error

--- frontend/app/stream/page.tsx
+++ frontend/app/stream/page.tsx
@@ -1,50 +1,37 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef } from 'react';
+import Video from 'next-video';
+import movie from '@/videos/file_example_MP4_1280_10MG.mp4';
+
 
 export default function StreamPage() {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  const play = async () => {
-    try {
-      if (videoRef.current) {
-        await videoRef.current.play();
+  const frameSend = () => {
+    const video = videoRef.current;
+    if (video) {
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      console.log(canvas.width, canvas.height);
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const frame = canvas.toDataURL('image/jpeg');
+        console.log(frame);
       }
-      const response = await fetch("http://localhost:5000/toggle_capture");
-      if (!response.ok) {
-        throw new Error('Failed to start streaming');
-      }
-    } catch (err) {
-      console.log('Error during play:', err);
-    }
-  };
-
-  const pause = async () => {
-    try {
-      if (videoRef.current) {
-        videoRef.current.pause();
-      }
-      const response = await fetch("http://localhost:5000/toggle_capture");
-      if (!response.ok) {
-        throw new Error('Failed to pause streaming');
-      }
-    } catch (err) {
-      console.log('Error during pause:', err);
     }
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-20">
-      <video controls width="1000" ref={videoRef}>
-        <source
-          src="http://localhost:5000/video/Panchayat.S03E01.720p.Hindi.WEB-DL.5.1.ESub.x264.Vegamovies.to.mkv"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
-      <button onClick={play}>Play</button>
-      <button onClick={pause}>Pause</button>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <Video
+        ref={videoRef}
+        controls
+        src={movie}
+        onPause={frameSend}
+      />
     </main>
   );
 }
-
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
+import { withNextVideo } from 'next-video/process';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withNextVideo(nextConfig, {
+  provider: 'vercel-blob',
+  providerConfig: {
+    'vercel-blob': {
+      generateAssetKey(filePath, folder) {
+        return folder + '/' + filePath;
+      },
+      
+    },
+  },
+  folder: 'videos',
+});
