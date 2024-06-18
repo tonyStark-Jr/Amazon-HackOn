d69b42775ba21f1bb6132c042b1c89d1e712e5d2
Harshal Gainer <harshalgainer5167@gmail.com>
2024-06-19 00:26:39 +0530

    fixed frame extractor && implemented sending to backend

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,51 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
+from flask_socketio import SocketIO, emit
+import os
+import time
+import base64
 
 app = Flask(__name__)
 CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@app.route('/')
+@app.route("/")
 def hello_world() -> str:
-    return 'Hello, World!'
+    return "Hello, World!"
 
-@app.route('/video/<path:subpath>')
+
+@socketio.on("connect")
+def handle_connect():
+    print("Client connected")
+    emit("message", {"data": "Connected to the server"})
+
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("Client disconnected")
+
+
+@socketio.on("send_data")
+def handle_send_data(data):
+    print(f"Received data from client")
+    image_data = data["data"].split(",")[1]
+    try:
+        decoded_data = base64.b64decode(image_data)
+        filename = f"static/image_{int(time.time())}.png"
+        with open(filename, "wb") as file:
+            file.write(decoded_data)
+        print("Image saved")
+
+    except Exception as err:
+        print(err)
+
+
+@app.route("/video/<path:subpath>")
 def stream_video(subpath: str):
-    return send_from_directory('videos', subpath)
+    return send_from_directory("static", subpath)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- frontend/app/stream/page.tsx
+++ frontend/app/stream/page.tsx
@@ -1,12 +1,16 @@
-"use client";
+'use client';
 
-import React, { useRef } from 'react';
-import Video from 'next-video';
-import movie from '@/videos/file_example_MP4_1280_10MG.mp4';
+import React, { useRef, useEffect } from 'react';
+import video from '@/videos/file_example_MP4_1280_10MG.mp4';
+import io, { Socket } from 'socket.io-client';
 
+interface ClientToServerEvents {
+  send_data: (data: { data: string }) => void;
+}
 
 export default function StreamPage() {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const socket = useRef<Socket<ClientToServerEvents> | null>(null);
 
   const frameSend = () => {
     const video = videoRef.current;
@@ -19,19 +23,34 @@ export default function StreamPage() {
       if (ctx) {
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
         const frame = canvas.toDataURL('image/jpeg');
-        console.log(frame);
+        socket.current?.emit('send_data', { data: frame });
       }
     }
   };
 
+  useEffect(() => {
+    socket.current = io('http://localhost:5000');
+
+    socket.current.on('connect', () => {
+      console.log('Connected to the server');
+    });
+
+    socket.current.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    return () => {
+      socket.current?.disconnect();
+    };
+  }, []);
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Video
-        ref={videoRef}
-        controls
-        src={movie}
-        onPause={frameSend}
-      />
+      {video.sources && (
+        <video crossOrigin='anonymous' controls ref={videoRef} onPause={frameSend}>
+          <source src={video.sources[0].src} type={video.sources[0].type} />
+        </video>
+      )}
     </main>
   );
 }
